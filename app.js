73f344928a0e9b43def7c0658c4bad6a7c9982f5
hcdclove <131356756+hcdclove@users.noreply.github.com>
2023-05-22 20:00:05 -0400

    Hided the API Access Key and other secret security detail

--- app.js
+++ app.js
@@ -16,30 +16,29 @@ const herokuPost = process.env.PORT;
 const localPort = 3000;
 const prefix = 'hcdc';
 
-// mailchimp.setConfig({
-//   accessToken: process.env.MC_API_KEY,
-//   server: process.env.MC_SERVER,
-// });
+mailchimp.setConfig({
+  accessToken: process.env.MC_API_KEY,
+  server: process.env.MC_SERVER,
+});
 
-// // // API Settings
-// const endPoint = process.env.MC_END_POINT;
-// const audience = process.env.MC_AUDIENCE;
+// // API Settings
+const endPoint = process.env.MC_END_POINT;
+const audience = process.env.MC_AUDIENCE;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// async function callPing() {
-//   const response = await mailchimp.ping.get();
-//   onsole.log(response.health_status);
-// }
+async function callPing() {
+  const response = await mailchimp.ping.get();
+  // console.log(response.health_status);
+}
 
-// test that  mailchimp is listening to us
+// test that mailchimp is listening to us
 // callPing();
 
 // Home route - serving the home page represented by index.html
 app.get('/', (req, res) => {
-  // res.sendFile(__dirname + '/views/signup.html');
-  res.send(process.env);
+  res.sendFile(__dirname + '/views/signup.html');
 });
 
 // Success route - serving the success page represented by success.html
@@ -53,50 +52,50 @@ app.get('/fealure', (req, res) => {
 });
 
 // Add a contact to my mailing list
-// app.post('/', (req, res) => {
-//   // Prepare the data to be sent to the mailchip server
-//   const fname = req.body.fname;
-//   const lname = req.body.lname;
-//   const email = req.body.email;
+app.post('/', (req, res) => {
+  // Prepare the data to be sent to the mailchip server
+  const fname = req.body.fname;
+  const lname = req.body.lname;
+  const email = req.body.email;
 
-//   const data = {
-//     members: [
-//       {
-//         email_address: email,
-//         status: 'subscribed',
-//         merge_fields: {
-//           FNAME: fname,
-//           LNAME: lname,
-//         },
-//       },
-//     ],
-//   };
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscribed',
+        merge_fields: {
+          FNAME: fname,
+          LNAME: lname,
+        },
+      },
+    ],
+  };
 
-//   const jsonData = JSON.stringify(data);
+  const jsonData = JSON.stringify(data);
 
-//   const url = endPoint + '/lists/' + audience;
+  const url = endPoint + '/lists/' + audience;
 
-//   const option = {
-//     method: 'POST',
-//     auth: prefix + ':' + process.env.MC_API_KEY + '-' + process.env.MC_SERVER,
-//   };
+  const option = {
+    method: 'POST',
+    auth: prefix + ':' + process.env.MC_API_KEY + '-' + process.env.MC_SERVER,
+  };
 
-//   // Connect to the mailchip server and send the data
-//   const request = https.request(url, option, function (response) {
-//     response.on('data', (data) => {
-//       const success = 200;
+  // Connect to the mailchip server and send the data
+  const request = https.request(url, option, function (response) {
+    response.on('data', (data) => {
+      const success = 200;
 
-//       if (response.statusCode == success) {
-//         res.sendFile(__dirname + '/views/success.html', (req, res) => {});
-//       } else {
-//         res.sendFile(__dirname + '/views/fealure.html', (req, res) => {});
-//       }
-//     });
-//   });
+      if (response.statusCode == success) {
+        res.sendFile(__dirname + '/views/success.html', (req, res) => {});
+      } else {
+        res.sendFile(__dirname + '/views/fealure.html', (req, res) => {});
+      }
+    });
+  });
 
-//   request.write(jsonData);
-//   request.end();
-// });
+  request.write(jsonData);
+  request.end();
+});
 
 app.post('/fealure', (req, res) => {
   res.redirect('/');
